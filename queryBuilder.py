4aa417c389ade6a48adcba25fcd108f89eeae14d
OllieMilky <oliver-hall@hotmail.co.uk>
2021-03-26 11:01:59 +0000

    Code tidy-up for handover

--- queryBuilder.py
+++ queryBuilder.py
@@ -7,48 +7,47 @@ numberList = []
 
 driver = webdriver.Firefox()
 
+#Tweet data is loaded in from the CSV, but only the actual Tweet content
 with open('tweetData.csv', encoding='mbcs') as file:
     reader = csv.reader(file)
 
     count = 0
     for row in reader:
-        contentList.append(row[4])
+        contentList.append(row[4]) # Just extract the content of each tweet, not the metadata
 
-        if count > 1560:
-            break 
+contentList = contentList[1:] # Remove the headings from the list
 
-        count +=1 
-
-contentList = contentList[1:]
-
+# Leading phrases loaded into phrases
 with open('leadingPhrases.csv', newline='') as f:
     reader = csv.reader(f)
     phrases = list(reader)
 
+# Function for tokenising words and looking for queries by comparing with leading phrases 
 for content in contentList:
     for phrase in phrases:
-        if phrase[0] in content:
-            tokens = nltk.word_tokenize(content)
-            tags = nltk.pos_tag(tokens)
-            for tag in tags:
-                if tag[1] == 'CD':
-                    print(content)
-                    preceedingPhrase = phrase[0] +" "+ tag[0]
+        if phrase[0] in content: # If leading phrase in tweet
+            tokens = nltk.word_tokenize(content) # Tokenise
+            tags = nltk.pos_tag(tokens) # POS tag
+            for tag in tags: # For each word/tag pair
+                if tag[1] == 'CD': # If word is a number
+                    print(content) # Print tweet
+                    preceedingPhrase = phrase[0] +" "+ tag[0] # Create possible matching queries
                     proceedingPhrase = tag[0] +" "+ phrase[0]
-                    if (preceedingPhrase in content):
+                    if (preceedingPhrase in content): # Check if matching phrase is in the Tweet
                         numberList.append(content)
                     if (proceedingPhrase in content):
                         numberList.append(content)
 
-numberList = set(numberList)
-for element in numberList:
+# Inputs each query into the google search engine before returning the top URLs of search results 
+numberList = set(numberList) # Removes duplicate data values
+for element in numberList: # For each query 
     print(element)
-    driver.get("https://www.google.com")
+    driver.get("https://www.google.com") # Go to Google
+    time.sleep(2) # Wait for webpage to load
+    searchBox = driver.find_element_by_name("q") # Find the search box
+    searchBox.send_keys(element, Keys.ENTER) # Input element into search box
     time.sleep(2)
-    searchBox = driver.find_element_by_name("q")
-    searchBox.send_keys(element, Keys.ENTER)
-    time.sleep(2)
-    posts = driver.find_elements_by_class_name("yuRUbf")
+    posts = driver.find_elements_by_class_name("yuRUbf") # Find post results in returned HTML
     for post in posts:
-        print(post.find_element_by_css_selector('a').get_attribute('href'))
+        print(post.find_element_by_css_selector('a').get_attribute('href')) # Print the URL 
 
--- seleniumScrape.py
+++ seleniumScrape.py
@@ -6,25 +6,26 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas
 
+#List of terms to be searched for on Twitter. These can be changed to suit subject matter
 searchTerms = ['COVID', 'Vaccine', 'COVID19', 'coronavirus', 'COVID-19']
 
-url = 'https://twitter.com/login'
-driver = webdriver.Firefox()
+url = 'https://twitter.com/login' # First navigation to Twitter
+driver = webdriver.Firefox() #Can be edited if using Chrome etc 
 
-
+#Used to derive the searching page while inputting the search terms as a hashtag
 def base_url(
     hashtag): return "https://twitter.com/search?q=lang%3Aen%20%23{}&src=typed_query&f=live".format(hashtag)
 
-
+#Function for trawling through the HTML of the page and collecting the correct elements
 def getTweetData(card, term):
 
     # Username
     Username = card.find_element_by_xpath('.//span').text
-    Username = Username.encode('ascii', 'ignore')
+    Username = Username.encode('ascii', 'ignore') # Usernames can contain strange characters so encoded to basic chars
     print()
-    Username = str(Username).replace("b'", "")
+    Username = str(Username).replace("b'", "") #Replace strange chars
     Username = str(Username).replace("\\n", "")
-    Username = Username[:-1]
+    Username = Username[:-1] #Remove final username character added by Twitter
     # print(Username)
 
     # Twitter Handle
@@ -62,28 +63,29 @@ def getTweetData(card, term):
     LikesCount = card.find_element_by_xpath('.//div[@data-testid="like"]').text
     print(LikesCount)
 
+#Data is then stored in a list before being returned
     tweet = (term, Username, Handle, PostDate, Content,
              ReplyCount, RetweetCount, LikesCount)
     return tweet
 
-
+#Function for inputting a new search term and scrolling down
 def scrapePage(localUrl, n, term, tweetData):
-    driver.get(localUrl)
-    driver.implicitly_wait(5)
-    driver.find_element_by_link_text('Latest').click()
+    driver.get(localUrl) # Access search URL
+    driver.implicitly_wait(5) # Wait for page to load
+    driver.find_element_by_link_text('Latest').click() # Navigate to 'latest' tab
     data = []
-    tweet_ids = set()
-    last_pos = driver.execute_script("return window.pageYOffset;")
+    tweet_ids = set() 
+    last_pos = driver.execute_script("return window.pageYOffset;") # Find last scrolling position
     scrolling = True
     scrollNo = 0
 
-    while scrolling:
-        cards = driver.find_elements_by_xpath('//div[@data-testid="tweet"]')
-        for card in cards:
+    while scrolling: # Continue to scroll, loading new Tweets
+        cards = driver.find_elements_by_xpath('//div[@data-testid="tweet"]') # Finds each tweet
+        for card in cards: # For each loaded tweet
             data = getTweetData(card, term)
-            if data:
+            if data: # If successful 
                 tweet_id = ''.join(data)
-                if tweet_id not in tweet_ids:
+                if tweet_id not in tweet_ids: # Used to check that Tweets aren't being added more than once
                     tweet_ids.add(tweet_id)
                     tweetData.append(data)
 
@@ -94,11 +96,11 @@ def scrapePage(localUrl, n, term, tweetData):
                 'window.scrollTo(0, document.body.scrollHeight);')
             scrollNo += 1
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + str(scrollNo))
-            if scrollNo >= n:
+            if scrollNo >= n: # Value of n can be changed to scroll more than once per search
                 scrolling = False
                 break
             time.sleep(3)
-            curr_position = driver.execute_script("return window.pageYOffset;")
+            curr_position = driver.execute_script("return window.pageYOffset;") # Continue to scroll
             if last_pos == curr_position:
                 scrollAttempt += 1
                 if scrollAttempt >= 3:
@@ -111,22 +113,23 @@ def scrapePage(localUrl, n, term, tweetData):
                 break
     return(tweetData)
 
-
+#Function for saving the tweets to a CSV. The commented out line is for use when an empty csv is being used/first set up
 def saveTweets(searchTerms, n):
     tweetList = []
     for term in searchTerms:
         print()
         print('Scraping for #{}'.format(term))
         tweetList = scrapePage(base_url(term), n, term, tweetList)
-    df = pandas.DataFrame(tweetList)
+    df = pandas.DataFrame(tweetList) # Pandas used to store tweets in a dataframe
     # df.to_csv('tweetData.csv', encoding='utf-8', index=False, header=[
     #           "Search Term", "Username", "Handle", "PostDate", "Content", "Comments", "Retweets", "Likes"])
     old = pandas.read_csv('tweetData.csv', header=None)
     old = old.append(df, ignore_index=True)
-    new = old.drop_duplicates()
-    new.to_csv('tweetData.csv',
+    new = old.drop_duplicates() # Duplicate tweets removed
+    new.to_csv('tweetData.csv', # New information stored in CSV format
                encoding='utf-8', index=False)
 
+#Function used to wait while the web pages are allowed to load
 def waiting_function(by_variable, attribute):
     try:
         WebDriverWait(driver, 20).until(
@@ -135,16 +138,16 @@ def waiting_function(by_variable, attribute):
         print(' {} {} not found'.format(by_variable, attribute))
         exit()
 
-
+#Main function - logs into Twitter and then continues to scrape tweets indefinitely
 if __name__ == '__main__':
     driver.get(url)
 
     waiting_function('name', 'session[username_or_email]')
     email = driver.find_element_by_name('session[username_or_email]')
     password = driver.find_element_by_name('session[password]')
-    email.send_keys('LaneTrance')
-    password.send_keys('PivotalData', Keys.ENTER)
+    email.send_keys('LaneTrance') # Username, can be edited
+    password.send_keys('PivotalData', Keys.ENTER) # Password for account, can be edited
 
     while True:
-        saveTweets(searchTerms, 1)
+        saveTweets(searchTerms, 1) # The number '1' can be changed so that you can scroll more than once per search
     driver.quit()
